b2f3f61c6570dc84c7997b53ebec4ce91b97f41d
Tomasz Aleksandrzak <t.aleksandrzak@o2.pl>
2022-10-09 14:28:13 +0200

    usunięcie numerów przy zawodnikach

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 let teams = []; // empty array for
 let i = 14;
 
-let n = Math.floor(Math.random() * 15) + 1; //generate random number 1-15
+let n = Math.floor(Math.random() * 15); //generate random number 0-14
 teams.push(n);
 
 // create array of 15 random integers
 while (i > 0) {
-  n = Math.floor(Math.random() * 15) + 1;
+  n = Math.floor(Math.random() * 15);
   if (!teams.includes(n)) {
     teams.push(n);
     i--;
   }
 }
-
-alert("test");
 
 // deviding array teams for 3 teams
 teamA = teams.slice(0, 5);
@@ -23,27 +21,31 @@ teamC = teams.slice(10, 15);
 function fun1() {
   const wejscie = document.getElementById("tbuser").value;
   const sklad = wejscie.split(",");
+  let sklad2 = [];
+  for (let i = 1; i <= 15; i++) {
+    sklad2.push(sklad[i].slice(4, sklad[i].length));
+  }
 
   document.querySelector(".teamA").textContent = `Drużyna A to:
-  ${sklad[teamA[0]]},
-  ${sklad[teamA[1]]},
-  ${sklad[teamA[2]]},
-  ${sklad[teamA[3]]},
-  ${sklad[teamA[4]]}`;
+  ${sklad2[teamA[0]]},
+  ${sklad2[teamA[1]]},
+  ${sklad2[teamA[2]]},
+  ${sklad2[teamA[3]]},
+  ${sklad2[teamA[4]]}`;
 
   document.querySelector(".teamB").textContent = `Drużyna B to:
-  ${sklad[teamB[0]]},
-  ${sklad[teamB[1]]},
-  ${sklad[teamB[2]]},
-  ${sklad[teamB[3]]},
-  ${sklad[teamB[4]]}`;
+  ${sklad2[teamB[0]]},
+  ${sklad2[teamB[1]]},
+  ${sklad2[teamB[2]]},
+  ${sklad2[teamB[3]]},
+  ${sklad2[teamB[4]]}`;
 
   document.querySelector(".teamC").textContent = `Drużyna C to:
-  ${sklad[teamC[0]]},
-  ${sklad[teamC[1]]},
-  ${sklad[teamC[2]]},
-  ${sklad[teamC[3]]},
-  ${sklad[teamC[4]]}`;
+  ${sklad2[teamC[0]]},
+  ${sklad2[teamC[1]]},
+  ${sklad2[teamC[2]]},
+  ${sklad2[teamC[3]]},
+  ${sklad2[teamC[4]]}`;
 }
 
 document.getElementById("btn1").addEventListener("click", fun1);
